023db0c5564ce6c21378b44f0ce72a0a3834bf47
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2021-05-16 14:02:25 +0200

    Projektdateien hinzufügen.

---
+++ MinecraftVideoConverter/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace MinecraftVideoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VideoSelect = new System.Windows.Forms.Button();
+            this.PreviewPicture = new System.Windows.Forms.PictureBox();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.StartConversion = new System.Windows.Forms.Button();
+            this.WorldSelection = new System.Windows.Forms.ComboBox();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.WorldPath = new System.Windows.Forms.Label();
+            this.VideoPath = new System.Windows.Forms.Label();
+            this.OpenWorldButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VideoSelect
+            // 
+            this.VideoSelect.Location = new System.Drawing.Point(64, 12);
+            this.VideoSelect.Name = "VideoSelect";
+            this.VideoSelect.Size = new System.Drawing.Size(120, 23);
+            this.VideoSelect.TabIndex = 0;
+            this.VideoSelect.Text = "Select Video";
+            this.VideoSelect.UseVisualStyleBackColor = true;
+            this.VideoSelect.Click += new System.EventHandler(this.SelectVideo_Click);
+            // 
+            // PreviewPicture
+            // 
+            this.PreviewPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PreviewPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PreviewPicture.Location = new System.Drawing.Point(12, 82);
+            this.PreviewPicture.Name = "PreviewPicture";
+            this.PreviewPicture.Size = new System.Drawing.Size(640, 384);
+            this.PreviewPicture.TabIndex = 1;
+            this.PreviewPicture.TabStop = false;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(12, 472);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(640, 23);
+            this.ProgressBar.TabIndex = 2;
+            // 
+            // StartConversion
+            // 
+            this.StartConversion.Location = new System.Drawing.Point(532, 501);
+            this.StartConversion.Name = "StartConversion";
+            this.StartConversion.Size = new System.Drawing.Size(120, 23);
+            this.StartConversion.TabIndex = 4;
+            this.StartConversion.Text = "Convert";
+            this.StartConversion.UseVisualStyleBackColor = true;
+            this.StartConversion.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // WorldSelection
+            // 
+            this.WorldSelection.DisplayMember = "name";
+            this.WorldSelection.FormattingEnabled = true;
+            this.WorldSelection.Location = new System.Drawing.Point(290, 12);
+            this.WorldSelection.Name = "WorldSelection";
+            this.WorldSelection.Size = new System.Drawing.Size(156, 23);
+            this.WorldSelection.TabIndex = 5;
+            this.WorldSelection.SelectedIndexChanged += new System.EventHandler(this.Worlds_SelectedIndexChanged);
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Location = new System.Drawing.Point(12, 498);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(22, 15);
+            this.ProgressLabel.TabIndex = 6;
+            this.ProgressLabel.Text = "---";
+            // 
+            // WorldPath
+            // 
+            this.WorldPath.AutoSize = true;
+            this.WorldPath.Location = new System.Drawing.Point(12, 61);
+            this.WorldPath.Name = "WorldPath";
+            this.WorldPath.Size = new System.Drawing.Size(102, 15);
+            this.WorldPath.TabIndex = 7;
+            this.WorldPath.Text = "No world selected";
+            // 
+            // VideoPath
+            // 
+            this.VideoPath.AutoSize = true;
+            this.VideoPath.Location = new System.Drawing.Point(12, 42);
+            this.VideoPath.Name = "VideoPath";
+            this.VideoPath.Size = new System.Drawing.Size(101, 15);
+            this.VideoPath.TabIndex = 8;
+            this.VideoPath.Text = "No video selected";
+            // 
+            // OpenWorldButton
+            // 
+            this.OpenWorldButton.Location = new System.Drawing.Point(504, 12);
+            this.OpenWorldButton.Name = "OpenWorldButton";
+            this.OpenWorldButton.Size = new System.Drawing.Size(148, 23);
+            this.OpenWorldButton.TabIndex = 9;
+            this.OpenWorldButton.Text = "Select World manually";
+            this.OpenWorldButton.UseVisualStyleBackColor = true;
+            this.OpenWorldButton.Click += new System.EventHandler(this.OpenWorldButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "World : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Video : ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(477, 501);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 23);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(367, 505);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Target frame rate :";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(664, 528);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpenWorldButton);
+            this.Controls.Add(this.VideoPath);
+            this.Controls.Add(this.WorldPath);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.WorldSelection);
+            this.Controls.Add(this.StartConversion);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.PreviewPicture);
+            this.Controls.Add(this.VideoSelect);
+            this.Name = "Form1";
+            this.Text = "Minecraft Video Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button VideoSelect;
+        private System.Windows.Forms.PictureBox PreviewPicture;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.Button StartConversion;
+        private System.Windows.Forms.ComboBox WorldSelection;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.Label WorldPath;
+        private System.Windows.Forms.Label VideoPath;
+        private System.Windows.Forms.Button OpenWorldButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MinecraftVideoConverter/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+using fNbt;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Numerics;
+using Emgu.CV.Cuda;
+using Emgu.CV.Ocl;
+
+namespace MinecraftVideoConverter {
+    public partial class Form1 :Form {
+        BackgroundWorker worker = new BackgroundWorker();
+        String VideoFilePath = null;
+        String WorldFolderPath = null;
+        int TargetFrameRate = 20;
+
+        class WorldItem {
+            public string name { get; set; }
+            public string path { get; set; }
+        }
+
+        public Form1() {
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += new DoWorkEventHandler(processVideo);
+            worker.ProgressChanged += new ProgressChangedEventHandler(updateProgress);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(conversionFinished);
+
+            InitializeComponent();
+
+            //update list of minecraft worlds in save folder
+            UpdateMinecraftWorlds();
+        }
+
+        private void UpdateMinecraftWorlds() {
+            List<WorldItem> worlds = new List<WorldItem>();
+
+            string minecraftSavesPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "\\.minecraft\\saves");
+            string[] paths = Directory.GetDirectories(minecraftSavesPath);
+
+            foreach (string dir in paths) {
+                //check if leveldata and levelname can be determined
+                try {
+                    NbtFile level = new NbtFile(Path.Join(dir, "/level.dat"));
+                    string name = level.RootTag.Get<NbtCompound>("Data").Get<NbtString>("LevelName").Value.ToString();
+                    worlds.Add(new WorldItem { name = name, path = dir });
+                } catch { }
+            }
+
+            WorldSelection.DataSource = worlds;
+        }
+
+        private void SelectVideo_Click(object sender, EventArgs e) {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK) {
+                VideoFilePath = Openfile.FileName;
+                VideoPath.Text = String.Concat("Video: \"", VideoFilePath, "\"");
+            }
+        }
+
+        private void updateProgress(object sender, ProgressChangedEventArgs e) {
+            ProgressLabel.Text = e.ProgressPercentage + "%   Remaining:" + Math.Round((double) e.UserState / 60000,2) +"minutes" ;
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+
+        private void conversionFinished(object sender, RunWorkerCompletedEventArgs e) {
+            convertResult result = (convertResult) e.Result;
+            MessageBox.Show("Video conversion finished! \n" +
+                "Note down the following values.\n" +
+                "Video framecount: " + result.framecount + "\n"+
+                "Video offset: " + result.offset);
+        }
+
+        double framerate, frames, reducedFrames;
+        long elapsedTime = 0;
+        double position = 0;
+        int frame = 0;
+
+        UMat singleFrame = new UMat(new Size(640, 384), DepthType.Cv32F, 3, UMat.Usage.AllocateDeviceMemory);
+        private void processVideo(object sender, DoWorkEventArgs e) {
+
+            CvInvoke.UseOpenCL = true;
+            CvInvoke.UseOptimized = true;
+
+            //determine amount of maps already present in the world
+            int mapCount = 0;
+            NbtFile map = null;
+            try {
+                map = new NbtFile(Path.Join(WorldFolderPath, "\\data\\idcounts.dat"));
+                mapCount = Int32.Parse(map.RootTag.Get<NbtCompound>("data").Get<NbtInt>("map").Value.ToString())+1;
+            } catch { }
+            Debug.Write("MapCount:" + mapCount + "\n");
+
+            //start videocapture
+            VideoCapture video = new VideoCapture(VideoFilePath);
+
+            //get framerate
+            framerate = video.GetCaptureProperty(CapProp.Fps);
+            //get framecount
+            frames = video.GetCaptureProperty(CapProp.FrameCount);
+
+            //calculate allFrames at the target framerate
+            reducedFrames = Math.Floor((frames / framerate) * TargetFrameRate);
+
+            if (map != null) {
+                map.RootTag.Get<NbtCompound>("data").Get<NbtInt>("map").Value = mapCount + ((int)reducedFrames) * 15;
+                map.SaveToFile(Path.Join(WorldFolderPath, "\\data\\idcounts.dat"), NbtCompression.None);
+            } 
+
+            //create Preset for map data
+            NbtCompound preset = new NbtCompound("") {
+                new NbtCompound("data"){
+                    new NbtString("dimension", "minecraft:overworld"),
+                    new NbtLong("xCenter", 128),
+                    new NbtLong("zCenter", 128),
+                    new NbtByte("scale", 3),
+                    new NbtByte("locked", 1),
+                    new NbtByte("unlimitedTracking", 0),
+                    new NbtByte("trackingPosition", 0),
+                    new NbtByteArray("colors")
+                },
+                new NbtInt("DataVersion",2584)
+            };
+
+            //create path to output folder
+            string mapOutputFolder = Path.Join(WorldFolderPath, "/data");
+            
+            UMat ones = new UMat(1, 3, DepthType.Cv8U, 1, UMat.Usage.AllocateDeviceMemory);
+            ones.SetTo(new MCvScalar(1));
+            
+
+            UMat calculation = new UMat(new Size(640, 384), DepthType.Cv32S, 3, UMat.Usage.AllocateDeviceMemory);
+            UMat singleChannel = new UMat(new Size(640, 384), DepthType.Cv32S, 1, UMat.Usage.AllocateDeviceMemory);
+            
+            //keeps lowest value
+            UMat lowestDiversion = new UMat(new Size(640, 384), DepthType.Cv32S, 1, UMat.Usage.AllocateDeviceMemory);
+            //bool
+            UMat lessDiversion = new UMat(new Size(640, 384), DepthType.Cv8U, 1, UMat.Usage.AllocateDeviceMemory);
+            //store block value
+            UMat blocks = new UMat(new Size(640, 384), DepthType.Cv8U, 1, UMat.Usage.AllocateDeviceMemory);
+
+
+            while (frame < reducedFrames) {
+                //calculate position in video and set to next frame
+                position = frame / reducedFrames;
+                video.SetCaptureProperty(CapProp.PosFrames, Math.Round(position * frames));
+
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                //get video frame
+                if (!video.Read(singleFrame)) break;
+
+                //resize to minecraft compatible resolution
+                CvInvoke.Resize(singleFrame, singleFrame, new Size(640, 384));
+                singleFrame.ConvertTo(singleFrame, DepthType.Cv32F);
+
+                //display current Frame to user
+                if (PreviewPicture.Image != null) PreviewPicture.Image.Dispose();
+                PreviewPicture.Image = singleFrame.ToBitmap();
+
+                lowestDiversion.SetTo(new MCvScalar(255));
+                lessDiversion.SetTo(new MCvScalar(0));
+                blocks.SetTo(Colors.minecraftColors[Colors.minecraftColors.Length-1]);
+
+                for (int i = 0; i < Colors.minecraftColors.Length; i++) {
+
+                    calculation = singleFrame - Colors.minecraftColors[i];
+                    CvInvoke.Multiply(calculation, calculation, calculation);
+                    CvInvoke.Transform(calculation, singleChannel, ones);
+
+                    CvInvoke.Sqrt(singleChannel, singleChannel);
+                    singleChannel.ConvertTo(singleChannel, DepthType.Cv32S);
+
+                    CvInvoke.Compare(singleChannel,lowestDiversion, lessDiversion, CmpType.LessThan);
+                
+                    singleChannel.CopyTo(lowestDiversion, lessDiversion);
+                
+                    blocks.SetTo(new MCvScalar(i + 4), lessDiversion);
+                }
+
+                for (int y = 0; y < 3; y++) {
+                    for (int x = 0; x < 5; x++) {
+                        UMat output = new UMat(blocks, new Rectangle(128 * x, 128 * y, 128, 128));
+                        preset.Get<NbtCompound>("data").Get<NbtByteArray>("colors").Value = output.Bytes;
+
+                        NbtFile file = new NbtFile(preset);
+                        file.SaveToFile(Path.Join(mapOutputFolder, String.Concat("map_",mapCount + (frame * 15) + (y * 5 + x),".dat")), NbtCompression.None);
+                    }
+                }
+                
+                watch.Stop();
+                elapsedTime = watch.ElapsedMilliseconds;
+                Debug.Write("Took:" + elapsedTime + "\n");
+
+                System.GC.Collect();
+
+                //send progress update to ui and console
+                worker.ReportProgress((int)Math.Round(position * 100), elapsedTime * (reducedFrames - frame));
+                Debug.Write(frame + "/" + reducedFrames + "-" + position + "\n");
+
+                //increase framecount
+                frame++;
+            }
+
+            worker.ReportProgress(100, 0.0);
+
+            //pass the values to display to the user
+            e.Result = new convertResult((int) frame-1, mapCount);
+        }
+
+        private struct convertResult {
+            public int framecount;
+            public int offset;
+
+            public convertResult(int _framecount,int _offset) {
+                framecount = _framecount;
+                offset = _offset;
+            }
+        }
+
+        private void Convert_Click(object sender, EventArgs e) {
+
+            //check if a video has been selected
+            if (VideoFilePath == null) {
+                MessageBox.Show("Please select a Video");
+                return;
+            }
+            //check a folder has been selected
+            if (WorldFolderPath == null) {
+                MessageBox.Show("Please select a World");
+                return;
+            }
+
+            //disable ui elements
+            WorldSelection.Enabled = false;
+            VideoSelect.Enabled = false;
+            OpenWorldButton.Enabled = false;
+            //start conversion worker
+            worker.RunWorkerAsync(argument: VideoFilePath);
+        }
+
+        private void Worlds_SelectedIndexChanged(object sender, EventArgs e) {
+            //update path on world selection
+            WorldFolderPath = ((WorldItem)((ComboBox)sender).SelectedItem).path;
+            UpdateWorldPath();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+            TargetFrameRate = (int) ((NumericUpDown)sender).Value;
+        }
+
+        private void OpenWorldButton_Click(object sender, EventArgs e) {
+            //open dialog for manual world selection
+            FolderBrowserDialog Openfolder = new FolderBrowserDialog();
+            if (Openfolder.ShowDialog() == DialogResult.OK) {
+                WorldFolderPath = Openfolder.SelectedPath;
+                UpdateWorldPath();
+            }
+        }
+
+        private void UpdateWorldPath() {
+            //update label text
+            WorldPath.Text = String.Concat("World: \"", WorldFolderPath, "\"");
+        }
+    }
+}
